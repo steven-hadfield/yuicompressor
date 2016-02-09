2af28b0e4f3f26e2a759e305303d6f9f57afdce1
Steven Hadfield <steven.hadfield@business.com>
2016-02-09 14:08:16 -0800

    Keep degree specifier for rotate animations

--- src/com/yahoo/platform/yui/compressor/CssCompressor.java
+++ src/com/yahoo/platform/yui/compressor/CssCompressor.java
@@ -330,9 +330,12 @@ public class CssCompressor {
 
         // Replace 0(px,em,%) with 0.
         css = css.replaceAll("(?i)(^|[^.0-9])(?:0?\\.)?0(?:px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz)", "$10");
-        css = css.replaceAll("(@[a-z\\-]*keyframe.*?)0\\{", "$10%{");
+
         // Replace x.0(px,em,%) with x(px,em,%).
         css = css.replaceAll("([0-9])\\.0(px|em|%|in|cm|mm|pc|pt|ex|deg|g?rad|m?s|k?hz| |;)", "$1$2");
+
+        css = css.replaceAll("(@[a-z\\-]*keyframe.*?)0\\{", "$10%{");
+        css = css.replaceAll("(rotate\\s*\\(\\s*0\\s*\\))", "rotate(0deg)");
 
         // Replace 0 0 0 0; with 0.
         css = css.replaceAll(":0 0 0 0(;|})", ":0$1");
